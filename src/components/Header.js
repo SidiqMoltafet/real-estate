3847b11e13b2f2f77b89244c9c444a84e8d986c0
Ahmad <moltafet.ahmad2015@gmail.com>
2023-02-18 18:08:57 -0800

    header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import react from 'react';
+
+
+function Header() {
+    let name = "hjvfjhv";
+  return (
+    <div>
+      <h2 >{name}</h2>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/util/Colors.js
@@ -0,0 +1,18 @@
+const LightColors = {
+    primary: '#FE4A49',
+    secondary: '#FED766',
+    background: '#FFFFFF',
+    text: '#000000',
+};
+
+const DarkColors = {
+    primary: '#222222',
+    secondary: '#009FB7',
+    background: '#FFFFFF',
+    text: '#FFFFFF',
+};
+
+export const Colors = {
+    light: LightColors,
+    dark: DarkColors,
+};
